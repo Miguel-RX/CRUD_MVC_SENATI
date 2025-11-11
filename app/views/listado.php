203acec669f9717d8b8914d7b41f27410bec231b
Miguel Angel <1640535@senati.pe>
2025-11-11 11:00:06 -0500

    APL BootsTrap y N-User

--- app/views/listado.php
+++ app/views/listado.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,16 +9,17 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     
     <link rel="stylesheet" href="estilos/CSS.css">
-<body>
+<body class="p-3 mb-2 bg-dark-subtle text-dark-emphasis">
     <br>
     <h1 class="display-4" >Listado de Usuarios</h1>
     <br>
 
-    <table class="table table-striped table-hover">
+    <table class="table table-striped">
         <tr">
             <td class="h5">Id</td>
             <td class="h5">Name</td>
             <td class="h5">LastName</td>
+            <td class="h5">Action</td>
         </tr>
 
         <?php foreach ($users as $user) { ?>
@@ -26,9 +27,13 @@
                 <td><?= $user['id']; ?></td>
                 <td><?= $user['name']; ?></td>
                 <td><?= $user['lastname']; ?></td>
+                <td>Editar | Eliminar</td>
             </tr>
         <?php } ?>
 
     </table>
+
+    <a href="/../CRUD_MVC_SENATI/app/views/agregar.php" type="button" class="btn btn-primary"> Nuevo + </a>
+
 </body>
 </html>
