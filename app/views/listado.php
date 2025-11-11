262384b5d0fc1ee08f58aa4a1f01d7a5da5c1653
Miguel Angel <1640535@senati.pe>
2025-11-11 10:28:11 -0500

    Estilos B

--- app/views/listado.php
+++ app/views/listado.php
@@ -3,18 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</head>
-<body>
-    <h1>Listado de Usuarios</h1>
+    <title>.: Listado :.</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     
-    <table border=1>
-        <tr>
-            <td>Id</td>
-            <td>Name</td>
-            <td>LastName</td>
+    <link rel="stylesheet" href="estilos/CSS.css">
+<body>
+    <br>
+    <h1 class="display-4" >Listado de Usuarios</h1>
+    <br>
+
+    <table class="table table-striped table-hover">
+        <tr">
+            <td class="h5">Id</td>
+            <td class="h5">Name</td>
+            <td class="h5">LastName</td>
         </tr>
 
         <?php foreach ($users as $user) { ?>
